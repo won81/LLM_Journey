38438da122cad2557ce515c38c874e1afda11a19
Jungwon Kim <jungwon81@gmail.com>
2023-09-26 22:51:41 +0900

    Adjust css for glossary
    Signed-off-by: Jungwon Kim <jungwon81@gmail.com>

--- pages/1_📘_Glossary.py
+++ pages/1_📘_Glossary.py
@@ -2,5 +2,17 @@ import streamlit as st
 
 st.set_page_config(page_title='Glossary - LLM Journey', page_icon=':rabbit:', layout='wide')
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("./pages/1_📘_Glossary.css")
+
 # Title
 st.title('📘 Glossary')
+
+with st.expander("LLM"):
+    st.markdown("""
+Large Languge Model
+대규모 언어 모델
+""")
