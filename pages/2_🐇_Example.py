1674193b5663c34cf9569c151901577f77c3a5cb
Jungwon Kim <jungwon81@gmail.com>
2023-10-06 03:50:14 +0000

    Add dummy examples
    Signed-off-by: Jungwon Kim <jungwon81@gmail.com>

--- pages/2_🐇_Example.py
+++ pages/2_🐇_Example.py
@@ -3,3 +3,15 @@ import streamlit as st
 st.set_page_config(page_title='Example - LLM Journey', page_icon=':rabbit:', layout='wide')
 
 st.title('🐇 Example')
+
+with st.expander("Zero-shot Prompting"):
+    st.markdown("""
+Large Languge Model
+대규모 언어 모델
+""")
+
+with st.expander("Few-shot Prompting"):
+    st.markdown("""
+Large Languge Model
+대규모 언어 모델
+""")
